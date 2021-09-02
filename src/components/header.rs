9ce76058ec9313c7d65386db699840d586c94af5
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-09-01 22:15:15 -0700

    NavLink

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,18 +1,25 @@
 use sycamore::prelude::*;
 
-#[component(Header<G>)]
-pub fn header() -> Template<G> {
+#[component(NavLink<G>)]
+pub fn nav_link((href, title): (&'static str, &'static str)) -> Template<G> {
     static LINK_CLASS: &str =
         "flex-1 text-center rounded hover:rounded-md hover:bg-gray-200 px-2 py-1 transition";
 
     template! {
+        a(class=LINK_CLASS, href=href) { (title) }
+    }
+}
+
+#[component(Header<G>)]
+pub fn header() -> Template<G> {
+    template! {
         nav(class="flex flex-row space-x-2 py-2") {
             h1(class="font-bold py-1") { "Hacker News Clone" }
 
-            a(class=LINK_CLASS, href="/") { "Top" }
-            a(class=LINK_CLASS, href="/new") { "Latest" }
-            a(class=LINK_CLASS, href="/best") { "Best" }
-            a(class=LINK_CLASS, href="/show") { "Show" }
+            NavLink(("/", "Top"))
+            NavLink(("/new", "Latest"))
+            NavLink(("/best", "Best"))
+            NavLink(("/show", "Show"))
         }
     }
 }
